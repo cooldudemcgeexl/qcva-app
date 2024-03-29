aee63a0902af7aa470fb92642ab085e9dd5a8406
Nick Lay <layns@mail.uc.edu>
2024-03-28 20:26:37 -0400

    stub out starting pages

--- client/constants/NavRoutes.ts
+++ client/constants/NavRoutes.ts
@@ -2,7 +2,15 @@ import { NavRoute } from "@/components/nav/NavDrawer"
 
 export const navRoutes: NavRoute[] = [
   {
-    name: "/(screens)/two",
+    name: "index",
     routeOptions: { drawerLabel: "Home", title: "Home" }
+  },
+  {
+    name: 'customers',
+    routeOptions: { drawerLabel: "Customers", title: "Customers" }
+  },
+  {
+    name: 'inventory',
+    routeOptions: { drawerLabel: "Inventory", title: "Inventory" }
   }
 ]
