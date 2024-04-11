fe99de0ccd282c008a1d80af34dfd0abaec9f159
Nick Lay <layns@mail.uc.edu>
2024-04-11 18:13:31 -0400

    add poledetail route

--- client/app/(screens)/inventory.tsx
+++ client/app/(screens)/inventory.tsx
@@ -1,11 +1,21 @@
 import InventoryTable from "@/components/Tables/InventoryTable";
+import { Link, router } from "expo-router";
 import { View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-
+import { Button, Icon, MD3Colors } from "react-native-paper";
 export default function InventoryScreen() {
   return (
     <View className="w-full flex-1 justify-center items-center">
-      <ScrollView className="w-full px-12 py-10">
+      <View className="py-4 self-end px-12 align-middle">
+        <Button
+          mode="contained"
+          icon="plus"
+          onPress={() => router.push("/poleDetails/")}
+        >
+          Add New
+        </Button>
+      </View>
+      <ScrollView className="w-full px-12">
         <InventoryTable />
       </ScrollView>
     </View>
---
+++ client/app/(screens)/poleDetails/index.tsx
@@ -0,0 +1,5 @@
+import { Button } from "react-native-paper";
+
+export function PoleDetails() {
+  return <Button>Text</Button>;
+}
